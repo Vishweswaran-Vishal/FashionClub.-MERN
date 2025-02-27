5ddde05e43246ea01d93f926ca7323ef8a8ed828
Vishweswaran-Vishal <vishweswaranvishal@gmail.com>
2025-02-27 16:14:28 +0530

    Initial commit

--- frontend/src/components/cart/Checkout.jsx
+++ frontend/src/components/cart/Checkout.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import PaypalButton from "./PayPalButton";
+import PaypalButton from "./PayPalButton.jsx";
 import { useDispatch, useSelector } from "react-redux";
 import { createCheckout } from "../../redux/slices/checkoutSlice";
 import axios from "axios";
